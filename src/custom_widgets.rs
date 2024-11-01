aad1fd04390220ebe63e6d3ae132658bf9f922a3
Purpl3 <dest4590@gmail.com>
2024-11-01 23:36:10 +0200

    Added custom widgets

---
+++ src/custom_widgets.rs
@@ -0,0 +1,29 @@
+use egui::CursorIcon::PointingHand as Clickable;
+
+pub trait Button {
+    fn cbutton(&mut self, label: &str) -> egui::Response;
+    fn button_with_tooltip(&mut self, label: &str, tooltip: &str) -> egui::Response;
+}
+
+impl Button for egui::Ui {
+    fn cbutton(&mut self, label: &str) -> egui::Response {
+        self.button(label).on_hover_cursor(Clickable)
+    }
+
+    fn button_with_tooltip(&mut self, label: &str, tooltip: &str) -> egui::Response {
+        self.button(label)
+            .on_hover_cursor(Clickable)
+            .on_hover_text(tooltip)
+    }
+}
+
+pub trait SelectableLabel {
+    fn cselectable_label(&mut self, checked: bool, text: &str) -> egui::Response;
+}
+
+impl SelectableLabel for egui::Ui {
+    fn cselectable_label(&mut self, checked: bool, text: &str) -> egui::Response {
+        self.selectable_label(checked, text)
+            .on_hover_cursor(Clickable)
+    }
+}
